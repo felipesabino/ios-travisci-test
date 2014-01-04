9b16d33bb4b5a29305e99ecbcdb3aca24886562a
felipesabino <felipe@sabino.me>
2014-01-04 13:59:59 -0200

    fixed keys decryption path in script

--- travis/decrypt-certs.sh
+++ travis/decrypt-certs.sh
@@ -2,5 +2,5 @@
 set -e
 
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/profile/Taqtile_Travis_Test_Adhoc.mobileprovision.enc -d -a -out ./travis/profile/Taqtile_Travis_Test_Adhoc.mobileprovision
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/certs/dist.p12.cer -d -a -out ./travis/certs/dist.p12
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/certs/dist.cer.p12 -d -a -out ./travis/certs/dist.p12
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/certs/dist.cer.enc -d -a -out ./travis/certs/dist.cer
