0f9fbd0ff2b79c4b8154171fcf5edae2eb36ef70
felipesabino <felipe@sabino.me>
2014-01-04 14:03:42 -0200

    renamed mobile provisioning after decrypt

--- travis/decrypt-certs.sh
+++ travis/decrypt-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/profile/Taqtile_Travis_Test_Adhoc.mobileprovision.enc -d -a -out ./travis/profile/Taqtile_Travis_Test_Adhoc.mobileprovision
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/profile/Taqtile_Travis_Test_Adhoc.mobileprovision.enc -d -a -out ./travis/profile/TravisCiIntegration.mobileprovision
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/certs/dist.cer.p12 -d -a -out ./travis/certs/dist.p12
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ./travis/certs/dist.cer.enc -d -a -out ./travis/certs/dist.cer
