b5527eb3f21ad9060b3b334b0e9017baf1a882b0
felipesabino <felipe@sabino.me>
2014-01-04 17:56:05 -0200

    tf test

--- travis/sign-and-upload.sh
+++ travis/sign-and-upload.sh
@@ -21,15 +21,15 @@ xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTP
 # Testflight upload
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
-RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER - Uploaded: $RELEASE_DATE"
 
 echo $RELEASE_NOTES
 
 curl http://testflightapp.com/api/builds.json \
-  -F file='@$OUTPUTDIR/$APP_NAME.ipa' \
-  -F dsym='@$OUTPUTDIR/$APP_NAME.app.dSYM' \
-  -F api_token='$TESTFLIGHT_API_TOKEN' \
-  -F team_token='$TESTFLIGHT_TEAM_TOKEN' \
+  -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM" \
+  -F api_token="$TESTFLIGHT_API_TOKEN" \
+  -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
   -F notify=true \
   -F distribution_lists='Internal' \
-  -F notes='$RELEASE_NOTES'
+  -F notes="$RELEASE_NOTES"
