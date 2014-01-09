afd465d8ea7f9c8cd362f9b1be0849ac12fc0888
felipesabino <felipe@sabino.me>
2014-01-09 12:59:35 -0200

    testing matrix with one item failing

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e
 
+if [[ "$BREAK" == "true" ]]; then
+  echo "BREAKING THIS BUILD TO TEST BUILD MATRIX SUCCESS CALLBACK"
+  exit 1
+fi
+
 xctool -workspace TravisCiIntegration.xcworkspace -scheme TravisCiIntegration -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
