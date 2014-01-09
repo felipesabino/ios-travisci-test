1179550d23812293022323834b049baf6fdba3d4
felipesabino <felipe@sabino.me>
2014-01-09 12:41:33 -0200

    reset build matrix tests

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -e
 
-echo "A: $A"
-echo "---"
-echo "B: $B"
-
-
 xctool -workspace TravisCiIntegration.xcworkspace -scheme TravisCiIntegration -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO
