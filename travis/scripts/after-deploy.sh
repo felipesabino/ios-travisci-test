7713f58ed110d4a804a7f83d81b6a0b48c8213fb
felipesabino <felipe@sabino.me>
2014-02-05 20:05:51 -0200

    using build asset path; upload stagin to s3 as well

---
+++ travis/scripts/after-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+echo "---------------------"
+echo "build uploaded to https://s3.amazonaws.com/$AMAZON_BUCKET/$AMAZON_UPLOAD_DIR/archive.zip"
+echo "---------------------"
--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -10,6 +10,8 @@ xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme "$AP
 
 echo "Generating $BUILD_ENVIRONMENT IPA file..."
 
-xcodebuild -verbose -exportArchive -exportFormat ipa -archivePath $BUILD_PATH/archive.xcarchive -exportPath $BUILD_PATH/build.ipa -exportProvisioningProfile "$PROFILE_NAME"
+mkdir "$BUILD_ASSETS_PATH"
 
-zip -r -9 "$BUILD_PATH/archive.zip" "$BUILD_PATH/archive.xcarchive"
+xcodebuild -verbose -exportArchive -exportFormat ipa -archivePath $BUILD_PATH/archive.xcarchive -exportPath $BUILD_ASSETS_PATH/build.ipa -exportProvisioningProfile "$PROFILE_NAME"
+
+zip -r -9 "$BUILD_ASSETS_PATH/archive.zip" "$BUILD_PATH/archive.xcarchive"
