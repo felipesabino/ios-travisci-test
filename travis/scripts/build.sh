de8574580e57fb4d1debf998e48833c094cd0927
felipesabino <felipe@sabino.me>
2014-02-05 16:26:19 -0200

    tesflight only deploys on correct build environment; added s3 deployment for production build environment

--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -6,8 +6,8 @@ echo "Usgin 'xcodebuild' instead of 'xctool' beacuse of https://github.com/faceb
 
 echo "Archiving $BUILD_ENVIRONMENT..."
 
-xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme "$APP_SCHEMA_BASE_NAME-$BUILD_ENVIRONMENT" -sdk iphoneos -configuration "Release-$BUILD_ENVIRONMENT" -archivePath $TRAVIS_BUILD_DIR/archive.xcarchive archive
+xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme "$APP_SCHEMA_BASE_NAME-$BUILD_ENVIRONMENT" -sdk iphoneos -configuration "Release-$BUILD_ENVIRONMENT" -archivePath $BUILD_PATH/archive.xcarchive archive
 
 echo "Generating $BUILD_ENVIRONMENT IPA file..."
 
-xcodebuild -verbose -exportArchive -exportFormat ipa -archivePath $TRAVIS_BUILD_DIR/archive.xcarchive -exportPath $TRAVIS_BUILD_DIR/build.ipa -exportProvisioningProfile "$PROFILE_NAME"
+xcodebuild -verbose -exportArchive -exportFormat ipa -archivePath $BUILD_PATH/archive.xcarchive -exportPath $BUILD_PATH/build.ipa -exportProvisioningProfile "$PROFILE_NAME"
--- travis/scripts/sign-and-upload.sh
+++ travis/scripts/sign-and-upload.sh
@@ -2,11 +2,15 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "This is a pull request. No deployment will be done."
+  echo "This is a pull request. No Testflight deployment will be done."
   exit 0
 fi
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  echo "Testing on a branch other than master. No deployment will be done."
+if [ "$TRAVIS_BRANCH" != "$TESTFLIGHT_DISTRIBUTION_BRANCH" ]; then
+  echo "Testing on a branch other than $TESTFLIGHT_DISTRIBUTION_BRANCH. No Testflight deployment will be done."
+  exit 0
+fi
+if [ "$BUILD_ENVIRONMENT" != "$TESTFLIGHT_BUILD_ENVIRONMENT" ]; then
+  echo "Not building $TESTFLIGHT_BUILD_ENVIRONMENT environment. Not Testflight deployment will be done."
   exit 0
 fi
 
@@ -15,9 +19,9 @@ fi
 TESTFLIGHT_RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 TESTFLIGHT_RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER - Uploaded: $TESTFLIGHT_RELEASE_DATE"
 
-TESTFLIGHT_IPA_PATH="$TRAVIS_BUILD_DIR/build.ipa"
-TESTFLIGHT_DSYM_PATH="$TRAVIS_BUILD_DIR/archive.xcarchive/dSYMs/$APP_PRODUCT_NAME.app.dSYM"
-TESTFLIGHT_DSYM_ZIP_PATH="$TRAVIS_BUILD_DIR/$APP_PRODUCT_NAME.app.dSYM.zip"
+TESTFLIGHT_IPA_PATH="$BUILD_PATH/build.ipa"
+TESTFLIGHT_DSYM_PATH="$BUILD_PATH/archive.xcarchive/dSYMs/$APP_PRODUCT_NAME.app.dSYM"
+TESTFLIGHT_DSYM_ZIP_PATH="$BUILD_PATH/$APP_PRODUCT_NAME.app.dSYM.zip"
 
 zip -r -9 "$TESTFLIGHT_DSYM_ZIP_PATH" "$TESTFLIGHT_DSYM_PATH"
 
@@ -27,5 +31,5 @@ curl http://testflightapp.com/api/builds.json \
   -F api_token="$TESTFLIGHT_API_TOKEN" \
   -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
   -F notify=true \
-  -F distribution_lists='Internal' \
+  -F distribution_lists="$TESTFLIGHT_DISTRIBUTION_LIST" \
   -F notes="$TESTFLIGHT_RELEASE_NOTES"
