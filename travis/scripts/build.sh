5dd5b5db5bc916637f26cada2f665557acb72c99
felipesabino <felipe@sabino.me>
2014-02-05 16:39:51 -0200

    s3 deploy using correct path and build number

--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -11,3 +11,5 @@ xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme "$AP
 echo "Generating $BUILD_ENVIRONMENT IPA file..."
 
 xcodebuild -verbose -exportArchive -exportFormat ipa -archivePath $BUILD_PATH/archive.xcarchive -exportPath $BUILD_PATH/build.ipa -exportProvisioningProfile "$PROFILE_NAME"
+
+zip -r -9 "$BUILD_PATH/archive.zip" "$BUILD_PATH/archive.xcarchive"
