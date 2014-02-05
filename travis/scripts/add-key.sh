353279b572dac359dcc51381cd5cd4d15139032c
felipesabino <felipe@sabino.me>
2014-02-05 12:56:53 -0200

    trying to use multiple keychains

--- travis/scripts/add-key.sh
+++ travis/scripts/add-key.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "Creating keychain..."
-security create-keychain -p travis ios-build.keychain
+echo "Creating keychains..."
+security create-keychain -p travis ios-build-production.keychain
+security create-keychain -p travis ios-build-staging.keychain
 
 echo "Adding apple certificate..."
-security import $PWD/travis/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import $PWD/travis/certs/apple.cer -k ~/Library/Keychains/ios-build-staging.keychain -T /usr/bin/codesign
+security import $PWD/travis/certs/apple.cer -k ~/Library/Keychains/ios-build-production.keychain -T /usr/bin/codesign
 
 echo "Adding certificates..."
-security import $PWD/travis/certs/dist.staging.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import $PWD/travis/certs/dist.production.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import $PWD/travis/certs/dist.staging.cer -k ~/Library/Keychains/ios-build-staging.keychain -T /usr/bin/codesign
+security import $PWD/travis/certs/dist.production.cer -k ~/Library/Keychains/ios-build-production.keychain -T /usr/bin/codesign
 
 echo "Adding private keys..."
-security import $PWD/travis/certs/dist.staging.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
-security import $PWD/travis/certs/dist.production.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import $PWD/travis/certs/dist.staging.p12 -k ~/Library/Keychains/ios-build-staging.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import $PWD/travis/certs/dist.production.p12 -k ~/Library/Keychains/ios-build-production.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 echo "Copying provisioning profiles..."
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
