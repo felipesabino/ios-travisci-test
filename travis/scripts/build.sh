c8b6ea768e1dea441f5d5ec65982947326345d34
felipesabino <felipe@sabino.me>
2014-02-05 15:30:17 -0200

    fixed sheme name

--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -6,7 +6,7 @@ echo "Usgin 'xcodebuild' instead of 'xctool' beacuse of https://github.com/faceb
 
 echo "Archiving $BUILD_ENVIRONMENT..."
 
-xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme $APP_SCHEMA_BASE_NAME -sdk iphoneos -configuration "Release-$BUILD_ENVIRONMENT" -archivePath $PWD/archive.xcarchive archive
+xcodebuild -verbose -workspace $APP_WORKSPACE_FILE_NAME.xcworkspace -scheme "$APP_SCHEMA_BASE_NAME-$BUILD_ENVIRONMENT" -sdk iphoneos -configuration "Release-$BUILD_ENVIRONMENT" -archivePath $PWD/archive.xcarchive archive
 
 echo "Generating $BUILD_ENVIRONMENT IPA file..."
 
